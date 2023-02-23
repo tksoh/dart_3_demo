263174260b2644187dbd2be2a3a4095dd517ff9a
TK Soh <teekaysoh@gmail.com>
2023-02-23 15:52:28 +0800

    add demo code for dart 3 records feature

--- lib/main.dart
+++ lib/main.dart
@@ -112,6 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Text('${geoLocation()}'),
           ],
         ),
       ),
@@ -122,4 +123,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+(double x, double y) geoLocation() {
+  return (-1.2921, 36.8219);
 }
